cef01f49af13003c24d9af99a0ecdc15cc2aadcf
J. vletter <99052114@mydavinci.nl>
2020-12-02 13:19:19 +0100

    added charmeleon + learned if

--- Pokebattle/index.php
+++ Pokebattle/index.php
@@ -2,5 +2,8 @@
 
 require 'pokemon.php';
 
-$pikachu = new pokemon('Pika', 'Electric', '100', 'pikapunch', 'fire', 'water');
+$pikachu = new pokemon('Pika', 'Electric', '60', 'Electric Ring', 'pikapunch', 'fire', 'water');
 print_r('<pre>'.$pikachu.'</pre>');
+
+$charmeleon = new pokemon('charcoal', 'Fire', '60', 'Head Butt', 'Flare', 'water', 'Lightning');
+print_r('<pre>'.$charmeleon.'</pre>');
--- Pokebattle/pokemon.php
+++ Pokebattle/pokemon.php
@@ -2,25 +2,54 @@
 
 class pokemon{
 	
-	
+	public $Name;
 	public $Energytype;
 	public $HP;
-	public $Attacks;
+	public $Attack1;
+	public $Attack2;
 	public $Weakness;
 	public $Resistance;
 	
 	
-	public function __construct($Name, $Energytype, $HP, $Attacks, $Weakness, $Resistance){
-		$this->name = $Name;
+	public function __construct($Name, $Energytype, $HP, $Attack1, $Attack2, $Weakness, $Resistance){
+		$this->Name = $Name;
 		$this->Energytype = $Energytype;
 		$this->HP = $HP;
-		$this->Attacks = $Attacks;
+		$this->Attack1 = $Attack1;
+		$this->Attack2 = $Attack2;
 		$this->Weakness = $Weakness;
 		$this->Resistance = $Resistance;
 		}
-	
+		
+		
+		
+		
 	public function __toString(){
 		return json_encode($this);
 		}
 	
 	}
+//reminder of all moves on the screen (will be deleted later)
+if ($Name = 'Pika'){
+	echo 'Moves for Pikachu'. '<br>';
+}
+
+if ($Attack1 = 'Electric Ring'){
+	echo 'Electric Ring deals 50 damage'. '<br>';
+}
+
+if ($Attack2 = 'Pikapunch'){
+	echo 'Pikapunch deals 20 damage'. '<br>'. '<br>';
+}
+
+if ($Name = 'charcoal'){
+	echo 'Moves for Charmeleon'. '<br>';
+}	
+
+if ($attack1 = 'Head Butt'){
+	echo 'Head Butt deals 10 damage'. '<br>';
+}
+
+if ($Attack2 = 'Flare'){
+	echo 'Flare deals 30 damage'. '<br>'. '<br>';
+}
